f7fbcfd0e61cb254c5e038a3a5426940c14e5f83
Rio Gunawan <ribbongrapes@gmail.com>
2025-12-23 17:39:13 +0900

    ドロワーメニューを追加

--- src/header.tsx
+++ src/header.tsx
@@ -1,11 +1,27 @@
 import iconUrl from './assets/imgs/icon.webp';
 import './assets/css/header.css';
+import { Drawer } from './drawer';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { useState } from 'preact/hooks';
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header>
-      <img alt='アイコン' src={iconUrl} width={64} />
-      外苑祭2026
-    </header>
+    <>
+      <header>
+        <img alt='アイコン' src={iconUrl} width={64} />
+        外苑祭2026
+      </header>
+
+      <button
+        className='menu-button'
+        onClick={() => setOpen(true)}
+        aria-label='メニュー'
+      >
+        <RxHamburgerMenu />
+      </button>
+      <Drawer isOpen={open} onClose={() => setOpen(false)} />
+    </>
   );
 };

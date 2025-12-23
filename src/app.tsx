2159efa22ab88b682da9a6ca271b69060ced4070
Rio Gunawan <ribbongrapes@gmail.com>
2025-12-23 16:33:16 +0900

    Update: ファーストビューと警告画面を実装

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,22 @@
-import './assets/css/app.css'
-import { Header } from './header'
+import { Alert } from './alert';
+import './assets/css/app.css';
+import { Header } from './header';
 
 export const App = () => {
   return (
     <>
-    <Header />
+      <Header />
+      <section className='first-view'>
+        <div className='first-view-content'>
+          <h1>外苑祭 2026</h1>
+          <p>2026/8/29~30 東京都立青山高校</p>
+        </div>
+      </section>
+      <Alert>
+        <p>
+          外苑祭は青山高校生徒から招待された人、または抽選で当選した中学生のみ参加できます。
+        </p>
+      </Alert>
     </>
-  )
-}
+  );
+};

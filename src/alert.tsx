0c63f695ea41f0baac63c23a47eb789695d87888
Rio Gunawan <ribbongrapes@gmail.com>
2025-12-23 16:33:16 +0900

    Update: ファーストビューと警告画面を実装

---
+++ src/alert.tsx
@@ -0,0 +1,14 @@
+import './assets/css/alert.css';
+import { IoWarning } from 'react-icons/io5';
+
+export const Alert = ({ children }: { children: preact.ComponentChildren }) => {
+  return (
+    <div className='alert'>
+      <h2>
+        <IoWarning />
+        注意
+      </h2>
+      {children}
+    </div>
+  );
+};
--- src/header.tsx
+++ src/header.tsx
@@ -4,7 +4,8 @@ import './assets/css/header.css';
 export const Header = () => {
   return (
     <header>
-      <img alt='アイコン' src={iconUrl} width={64} />外苑祭2026
+      <img alt='アイコン' src={iconUrl} width={64} />
+      外苑祭2026
     </header>
   );
 };

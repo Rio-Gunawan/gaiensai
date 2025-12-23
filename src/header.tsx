4ed13b384d0003c9b8eb9d149b9cd0fcffa91296
Rio Gunawan <ribbongrapes@gmail.com>
2025-12-23 15:31:02 +0900

    Add: Add header component.
    ヘッダーコンポーネントを追加。

---
+++ src/header.tsx
@@ -0,0 +1,10 @@
+import iconUrl from './assets/imgs/icon.webp';
+import './assets/css/header.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <img alt='アイコン' src={iconUrl} width={64} />外苑祭2026
+    </header>
+  );
+};

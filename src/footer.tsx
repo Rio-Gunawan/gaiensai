dec14f50f5609084cf9e87852140a3c4bc6a39df
Rio Gunawan <ribbongrapes@gmail.com>
2025-12-24 13:43:01 +0900

    Update: ナビゲーションコンポーネントを追加し、フッターとドロワーで使用

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,14 +1,10 @@
 import './assets/css/footer.css';
+import { UrlsNav } from './urls-nav';
 
 export const Footer = () => {
   return (
     <footer>
-      <ul>
-        <li><a href='#'>ホーム</a></li>
-        <li><a href='#'>スケジュール</a></li>
-        <li><a href='#'>アクセス</a></li>
-        <li><a href='#'>お問い合わせ</a></li>
-      </ul>
+      <UrlsNav />
       <p>最終更新日: 2025/12/23 チケットシステムver 0.0.1</p>
       <p>© 2026 外苑祭総務</p>
     </footer>

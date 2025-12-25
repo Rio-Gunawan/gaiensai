deebaf6e975a1e34bc5f42d4395110e4701291c7
Rio Gunawan <ribbongrapes@gmail.com>
2025-12-25 21:56:40 +0900

    Add: ルーティング機能と新しいページを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { render } from 'preact';
 import './assets/css/index.css';
 import { App } from './app.tsx';
+import { Ticket } from './ticket.tsx';
+import { Performances } from './performances.tsx';
+import { Route, Switch } from 'wouter-preact';
 
-render(<App />, document.getElementById('app')!);
+render(
+  <Switch>
+    <Route path='/' component={App} />
+    <Route path='/t' component={Ticket} />
+    <Route path='/performances' component={Performances} />
+    <Route>404 Not Found</Route>
+  </Switch>,
+  document.getElementById('app')!
+);

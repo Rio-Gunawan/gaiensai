e6bff0f237645f4130148e649a64f51ae4628bfb
Rio Gunawan <ribbongrapes@gmail.com>
2025-12-28 19:14:59 +0900

    Update: LINEでログイン機能の実装

--- src/students/student.tsx
+++ src/students/student.tsx
@@ -1,16 +1,16 @@
 import { Footer } from '../components/layout/footer';
 import { Header } from '../components/layout/header';
-import { Login } from "./login";
+import { Login } from './login';
 import '../assets/css/sub-pages.css';
 
 export const Students = () => {
-
   return (
     <>
       <Header />
       <main className='app-main'>
         <h1>生徒用ページ</h1>
         <Login />
+        <p></p>
       </main>
       <Footer />
     </>

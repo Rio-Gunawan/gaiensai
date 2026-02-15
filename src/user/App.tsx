c7ef63695e03ed1165235927018a5e84a3c49207
Rio Gunawan <ribbongrapes@gmail.com>
2026-02-15 13:35:41 +0900

    ページ遷移時に自動的にページトップへ移動するように変更。また、サブページにはHeader分の余白を追加。

--- src/user/App.tsx
+++ src/user/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Switch } from 'wouter-preact';
+import { useEffect } from 'preact/hooks';
+import { Route, Switch, useLocation } from 'wouter-preact';
 
 import MainLayout from '../layout/MainLayout';
 import { Home } from './pages/Home';
@@ -6,21 +7,38 @@ import { LineCallback } from '../features/auth/Line';
 import { Performances } from './pages/Performances';
 import { Student } from './pages/students/Student';
 import { Ticket } from './pages/Ticket';
+import subPageStyles from '../styles/sub-pages.module.css';
 
 import '../styles/index.css';
 import '../styles/color-settings.css';
 
+const ScrollToTop = () => {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [location]);
+
+  return null;
+};
+
 const App = () => {
+  const [location] = useLocation();
+  const isHome = location === '/';
+
   return (
     <MainLayout>
-      <Switch>
-        <Route path='/' component={Home} />
-        <Route path='/t' component={Ticket} />
-        <Route path='/performances' component={Performances} />
-        <Route path='/students' component={Student} />
-        <Route path='/auth/line/callback' component={LineCallback} />
-        <Route>404 Not Found</Route>
-      </Switch>
+      <ScrollToTop />
+      <div className={isHome ? '' : subPageStyles.subPageShell}>
+        <Switch>
+          <Route path='/' component={Home} />
+          <Route path='/t' component={Ticket} />
+          <Route path='/performances' component={Performances} />
+          <Route path='/students' component={Student} />
+          <Route path='/auth/line/callback' component={LineCallback} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </div>
     </MainLayout>
   );
 };

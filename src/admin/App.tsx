3ee5d06e62f9fb40d145c576618ed1e6731844a9
Rio Gunawan <ribbongrapes@gmail.com>
2026-03-09 23:00:07 +0900

    スキャンページに直接アクセスできない問題に対応その2

--- src/admin/App.tsx
+++ src/admin/App.tsx
@@ -6,41 +6,36 @@ import '../styles/color-settings.css';
 import '../styles/index.css';
 
 import subPageStyles from '../styles/sub-pages.module.css';
-import { Route, Router, Switch, useLocation } from 'wouter-preact';
+import { Route, Router, Switch } from 'wouter-preact';
 import NotFound from '../shared/NotFound';
 import AdminHome from './pages/AdminHome';
 import Scan from './pages/Scan';
 import ScanLayout from '../layout/ScanLayout';
 
 const App = () => {
-  const [location] = useLocation();
-  const isScanPage = location.startsWith('/admin/scan');
-
-  if (isScanPage) {
-    return (
-      <Router base='/admin/scan'>
-        <ScanLayout>
-          <ScrollToTop />
-          <Switch>
-            <Route path='/' component={Scan} />
-            <Route component={NotFound} />
-          </Switch>
-        </ScanLayout>
-      </Router>
-    );
-  }
-
   return (
     <Router base='/admin'>
-      <AdminLayout>
-        <ScrollToTop />
-        <div className={subPageStyles.subPageShell}>
-          <Switch>
-            <Route path='/' component={AdminHome} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </AdminLayout>
+      <Switch>
+        <Route path='/scan'>
+          <ScanLayout>
+            <ScrollToTop />
+            <Scan />
+          </ScanLayout>
+        </Route>
+
+        <Route path='/'>
+          <AdminLayout>
+            <ScrollToTop />
+            <div className={subPageStyles.subPageShell}>
+              <AdminHome />
+            </div>
+          </AdminLayout>
+        </Route>
+
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </Router>
   );
 };
